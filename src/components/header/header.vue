027ba794c13b3baa991eae2236c02866dcdb8dce
yy2016831 <1814186055@qq.com>
2018-01-19 18:44:18 +0800

    Error code
    有出错，待修复

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -17,20 +17,57 @@
           <span class="text">{{seller.supports[0].description}}</span>
         </div>
       </div>
+      <div v-if="seller.supports" class="support-count" @click="showDetail">
+        <span class="count">{{seller.supports.length}}个</span>
+        <i class="icon-keyboard_arrow_right"></i>
+      </div>
     </div>
-    <div class="bulletin-wrapper"></div>
+    <div class="bulletin-wrapper" @click="showDetail">
+      <span class="bulletin-title"></span>
+      <span class="bulletin-text">{{seller.bulletin}}</span>
+      <i class="icon-keyboard_arrow_right"></i>
+    </div>
+    <div class="background">
+      <img :src="seller.avatar">
+    </div>
+    <div v-show="detailShow" class="detail">
+      <div class="detail-wrapper clearfix">
+        <div class="detail-main">
+          <h1 class="name">{{seller.name}}</h1>
+          <star :size="48" :score="seller.score"></star>
+        </div>
+      </div>
+      <div class="detail-close">
+        <i class="icon-close"></i>
+      </div>
+    </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import star from '../star/star';
+
   export default {
     props: {
       seller: {
         type: Object
       }
     },
+    data() {
+      return {
+        detailShow: false
+      };
+    },
+    methods: {
+      showDetail() {
+        this.detailShow = true;
+      }
+    },
     created() {
       this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
+    },
+    components: {
+      star
     }
   };
 </script>
@@ -40,9 +77,12 @@
   @import "../../common/stylus/mixin";
 
   .header
+    position: relative;
+    overflow: hidden;
     color: #fff;
-    background: #000;
+    background: rgba(7, 17, 27, .5);
     .content-wrapper
+      position: relative;
       padding: 24px 12px 18px 24px;
       font-size: 0;
       .avatar
@@ -75,12 +115,13 @@
           line-height: 12px;
           font-size: 12px;
         .supports
+          font-size: 0;
           .icon
             display: inline-block;
             width: 12px;
             height 12px;
             margin-right: 4px;
-            vertical-align: middle;
+            vertical-align: top;
             background-size: 12px 12px;
             background-repeat: no-repeat;
             &.decrease
@@ -93,4 +134,89 @@
               bg-img('invoice_1');
             &.special
               bg-img('special_1');
+          .text
+            line-height: 12px;
+            font-size: 10px;
+      .support-count
+        position: absolute;
+        right: 12px;
+        bottom: 14px;
+        padding: 0 8px;
+        height: 24px;
+        line-height: 24px;
+        border-radius: 14px;
+        background: rgba(0, 0, 0, .2);
+        text-align: center;
+        .count
+          vertical-align: top;
+          font-size: 10px;
+        .icon-keyboard_arrow_right
+          line-height: 24px;
+          margin-left: 2px;
+          font-size: 10px;
+    .bulletin-wrapper
+      position: relative;
+      height: 28px;
+      line-height: 28px;
+      padding: 0 22px 0 12px;
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      background: rgba(7, 17, 27, .2);
+      .bulletin-title
+        display: inline-block;
+        vertical-align: top;
+        width: 22px;
+        height: 12px;
+        margin-top: 8px;
+        bg-img('bulletin');
+        background-size: 22px 12px;
+        background-repeat: no-repeat;
+      .bulletin-text
+        vertical-align: top;
+        margin: 0 2px;
+        font-size: 10px;
+      .icon-keyboard_arrow_right
+        position: absolute;
+        right: 12px;
+        top: 8px;
+        font-size: 10px;
+    .background
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      z-index: -1;
+      filter: blur(10px);
+      & > img
+        width: 100%;
+        height: 100%;
+    .detail
+      position: fixed;
+      top: 0;
+      left: 0;
+      z-index: 100;
+      width: 100%;
+      height: 100%;
+      overflow: auto;
+      background: rgba(7, 17, 27, .8);
+      .detail-wrapper
+        width: 100%;
+        min-height: 100%;
+        .detail-main
+          margin-top: 64px;
+          padding-bottom: 64px;
+          .name
+            line-height: 16px;
+            text-align: center;
+            font-size: 16px;
+            font-weigth: 700;
+      .detail-close
+        position: relative;
+        width: 32px;
+        height: 32px;
+        margin: -64px auto 0 auto;
+        clear: both;
+        font-size: 32px;
 </style>
